f000ada03b6c0e3d4b942ca8e6759556fe811deb
Alfred Peter <alpeter91@gmail.com>
2023-08-24 20:43:40 +0300

    Add nav links

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -2,6 +2,13 @@ import { Link, NavLink } from 'react-router-dom';
 import './sidebar.scss';
 import LogoS from '../../assets/images/logo-s.png';
 import LogoSubtitle from '../../assets/images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faEnvelope,
+  faHome,
+  faUser,
+  faX
+} from '@fortawesome/free-solid-svg-icons';
 const Sidebar = () => {
   return (
     <div className="nav-bar">
@@ -9,6 +16,19 @@ const Sidebar = () => {
         <img src={LogoS} alt="logo" />
         <img className="sub-logo" src={LogoSubtitle} alt="alfred" />
       </Link>
+      <nav>
+        <NavLink exact="true" to="/">
+          <FontAwesomeIcon icon={faHome} />
+        </NavLink>
+
+        <NavLink className="about-link" exact="true" to="/about">
+          <FontAwesomeIcon icon={faUser} />
+        </NavLink>
+
+        <NavLink className="contact-link" exact="true" to="/contact">
+          <FontAwesomeIcon icon={faEnvelope} />
+        </NavLink>
+      </nav>
     </div>
   );
 };
