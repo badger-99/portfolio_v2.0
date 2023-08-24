7612c505c29c2f430cbbe2dd4cc8daa1066a1e9f
Alfred Peter <alpeter91@gmail.com>
2023-08-24 23:20:02 +0300

    Add home page elements

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+import './home.scss';
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm
+          <img src={LogoTitle} alt="developer" />
+          lobodan
+          <br />
+          Web developer
+        </h1>
+        <h2>Full-stack developer</h2>
+        <Link className="flat-button" to="/contact">
+          Contact Me
+        </Link>
+      </div>
+    </div>
+  );
+};
+export default Home;
