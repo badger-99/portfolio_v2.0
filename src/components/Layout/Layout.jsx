fa49711afe7c044ea14a9ca65f6921b77fe712ba
Alfred Peter <alpeter91@gmail.com>
2023-08-24 21:43:47 +0300

    display html tags on webpage

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from 'react-router';
 import Sidebar from '../Sidebar/Sidebar';
 import './layout.scss';
 
@@ -5,7 +6,17 @@ const Layout = () => {
   return (
     <div>
       <Sidebar />
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+
+        <Outlet />
+        <span className="tags bottom-tags">
+          &lt;body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
     </div>
-  )
-}
-export default Layout
+  );
+};
+export default Layout;
