89f4a2c4caba9a0d24582a0d89cd4208dd3f9f64
Alfred Peter <alpeter91@gmail.com>
2023-08-24 20:42:13 +0300

    Add logo

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,1 +1,15 @@
+import { Link, NavLink } from 'react-router-dom';
 import './sidebar.scss';
+import LogoS from '../../assets/images/logo-s.png';
+import LogoSubtitle from '../../assets/images/logo_sub.png';
+const Sidebar = () => {
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="alfred" />
+      </Link>
+    </div>
+  );
+};
+export default Sidebar;
