bd45f0beb6dbc3ac540624fe1fa202044083de98
Alfred Peter <alpeter91@gmail.com>
2023-08-24 23:20:15 +0300

    Render home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Route, Routes } from 'react-router';
 import Layout from './components/Layout/Layout';
-import './App.scss'
+import Home from './components/Home/Home';
+import './App.scss';
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={< Layout/>} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
     </>
-  )
-}
-export default App
+  );
+};
+export default App;
