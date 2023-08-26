52cde5d8d3f5439a681163ba57db91b9269875d0
Alfred Peter <alpeter91@gmail.com>
2023-08-26 09:46:31 +0300

    Add loader to contact page

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,12 @@
+import Loader from 'react-loaders';
+import './contact.scss';
+
+const Contact = () => {
+  return (
+    <>
+      <div>Contact</div>
+      <Loader type='pacman'/>
+    </>
+  )
+}
+export default Contact
