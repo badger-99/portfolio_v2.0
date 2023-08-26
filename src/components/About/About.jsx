ff21c695dfa94b4865d11519984361d2f45685a9
Alfred Peter <alpeter91@gmail.com>
2023-08-26 03:39:39 +0300

    Add pacman loader

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
-import './about.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCss3, faGitAlt, faHtml5, faJsSquare, faPython, faReact, faSass } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
+import './about.scss';
 
 const About = () => {
   const [theClassName, setTheClassName] = useState('text-animate-hover');
@@ -14,59 +15,62 @@ const About = () => {
   }, []);
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            charArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
-            charClassName={theClassName}
-            idx={15}
-          />
-        </h1>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim debitis
-          adipisci nemo. Vitae deserunt odit omnis quam nesciunt, voluptatem
-          fugit.
-        </p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim debitis
-          adipisci nemo. Vitae deserunt odit omnis quam nesciunt, voluptatem
-          fugit.
-        </p>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim debitis
-          adipisci nemo. Vitae deserunt odit omnis quam nesciunt, voluptatem
-          fugit.
-        </p>
-      </div>
-      <div className="stage-cube-cont">
-        <div className="cube-spinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faHtml5} color="#f06529" />
-          </div>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              charArray={['A', 'b', 'o', 'u', 't', ' ', 'M', 'e']}
+              charClassName={theClassName}
+              idx={15}
+            />
+          </h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim
+            debitis adipisci nemo. Vitae deserunt odit omnis quam nesciunt,
+            voluptatem fugit.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim
+            debitis adipisci nemo. Vitae deserunt odit omnis quam nesciunt,
+            voluptatem fugit.
+          </p>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim
+            debitis adipisci nemo. Vitae deserunt odit omnis quam nesciunt,
+            voluptatem fugit.
+          </p>
+        </div>
+        <div className="stage-cube-cont">
+          <div className="cube-spinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faHtml5} color="#f06529" />
+            </div>
 
-          <div className="face2">
-            <FontAwesomeIcon icon={faCss3} color="#28a4d9" />
-          </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faCss3} color="#28a4d9" />
+            </div>
 
-          <div className="face3">
-            <FontAwesomeIcon icon={faJsSquare} color="#efd81d" />
-          </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faJsSquare} color="#efd81d" />
+            </div>
 
-          <div className="face4">
-            <FontAwesomeIcon icon={faReact} color="#5ed4f4" />
-          </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ed4f4" />
+            </div>
 
-          <div className="face5">
-            <FontAwesomeIcon icon={faGitAlt} color="#ec4d28" />
-          </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faGitAlt} color="#ec4d28" />
+            </div>
 
-          <div className="face6">
-            <FontAwesomeIcon icon={faSass} color="#c69" />
+            <div className="face6">
+              <FontAwesomeIcon icon={faSass} color="#c69" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Loader type='pacman'/>
+    </>
   );
 };
 export default About;
