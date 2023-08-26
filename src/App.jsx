0a27aa77dddc3f92b44eb64ad0081ddb6907fd80
Alfred Peter <alpeter91@gmail.com>
2023-08-26 09:42:07 +0300

    Add route to contact page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { Route, Routes } from 'react-router';
 import Layout from './components/Layout/Layout';
 import Home from './components/Home/Home';
-import './App.scss';
 import About from './components/About/About';
+import Contact from './components/Contact/Contact';
+import './App.scss';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
         </Route>
       </Routes>
     </>
