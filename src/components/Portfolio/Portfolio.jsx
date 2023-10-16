869c6d9096ce98735c6e3193da57fea18027aa54
Alfred Peter <alpeter91@gmail.com>
2023-10-16 23:45:54 +0300

    Add text animation to Portfolio page title

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Loader from "react-loaders";
+import AnimatedLetters from "../AnimatedLetters/AnimatedLetters";
+import "./portfolio.scss"
 
 const Portfolio = () => {
+  const word = 'Portfolio'.split('');
+  const [theClassName, setTheClassName] = useState('text-animate-hover');
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTheClassName('text-animate-hover');
+    }, 3000);
+  }, []);
   return (
     <>
-      <h1>Portfolio</h1>
+      <div className="container portfolio-page">
+        <h1 className="page-title">
+          <AnimatedLetters
+            charArray={word}
+            charClassName={theClassName}
+            idx={15}
+          />
+        </h1>
+      </div>
       <Loader type="pacman" />
     </>
-  )
+  );
 }
 export default Portfolio
