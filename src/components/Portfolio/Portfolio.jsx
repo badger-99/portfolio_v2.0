3ed81bd508515d7285189eefd9545b914db77897
Alfred Peter <alpeter91@gmail.com>
2023-11-15 20:09:08 +0300

    Add portfolio jsx code

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,16 +2,49 @@ import React, { useEffect, useState } from "react";
 import Loader from "react-loaders";
 import AnimatedLetters from "../AnimatedLetters/AnimatedLetters";
 import "./portfolio.scss"
+import portfolioData from "../../Data/portfolio.json";
 
 const Portfolio = () => {
   const word = 'Portfolio'.split('');
   const [theClassName, setTheClassName] = useState('text-animate-hover');
 
   useEffect(() => {
-    setTimeout(() => {
+    const timer = setTimeout(() => {
       setTheClassName('text-animate-hover');
     }, 3000);
-  }, []);
+
+    return () => {
+      clearTimeout(timer)
+    }
+  });
+
+  const renderPortfolio = (portfolio) => { 
+    return (
+      <div className="images_container">
+      {
+        portfolio.map((project, idx) => {
+          return (
+            <div className="image_box" key={idx}>
+              <img
+                src={project.Cover}
+                alt={project.Title}
+                className="portfolio-image"
+              />
+              <div className="content">
+                <p className="title">{project.Title}</p>
+                <h4 className="description">{project.Description}</h4>
+                <button className="btn" onClick={() => window.open(project.Demo)}>
+                  View
+                </button>
+              </div>
+            </div>
+          );
+        })
+      }
+      </div>
+    )
+  }
+  
   return (
     <>
       <div className="container portfolio-page">
@@ -22,6 +55,7 @@ const Portfolio = () => {
             idx={15}
           />
         </h1>
+        <div>{renderPortfolio(portfolioData.portfolio)}</div>
       </div>
       <Loader type="pacman" />
     </>
